94c81986d4f032a26147f3211e5dcf3474d3d5ea
Jane Greene <janegreenecasa@gmail.com>
2020-03-18 13:30:40 -0600

    Initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,16 @@
+class Restaurant
+  attr_reader :opening_time,
+              :name,
+              :dishes
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hours)
+    
+  end
+
+end
